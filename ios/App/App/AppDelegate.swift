e7f0fdef7f3b9a56b2cc757d8342d693aa581636
Hotdoog1234 <robert_francis@shieldmw.com>
2026-05-26 09:06:47 -0400

    Fix white screen: set native WKWebView and LaunchScreen background to dark green

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -8,6 +8,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if let vc = self.window?.rootViewController as? CAPBridgeViewController {
+            _ = vc.view // force viewDidLoad so webView is initialized
+            vc.webView?.backgroundColor = UIColor(red: 0.102, green: 0.290, blue: 0.180, alpha: 1.0)
+            vc.webView?.scrollView.backgroundColor = UIColor(red: 0.102, green: 0.290, blue: 0.180, alpha: 1.0)
+            vc.webView?.isOpaque = false
+        }
         return true
     }
 
