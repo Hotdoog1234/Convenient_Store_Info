e54004920a48d554fb4939c7587e5a45914f2f84
Hotdoog1234 <robert_francis@shieldmw.com>
2026-05-26 08:51:32 -0400

    Add HTML loading screen to prevent blank white screen on cold launch

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+document.getElementById('initial-loader')?.remove();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
